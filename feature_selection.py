9b51219addf916ff9b50985345a0138e638645ef
dcoelho045 <dcoelho045@gmail.com>
2024-06-09 21:22:42 -0700

    fixed feature_selecitons

--- feature_selection.py
+++ feature_selection.py
@@ -2,6 +2,7 @@ import random
 from sklearn.decomposition import TruncatedSVD
 import numpy as np
 
+
 class FeatureSelection:
     def __init__(self, validator, classifier):
         self.validator = validator
@@ -11,36 +12,36 @@ class FeatureSelection:
         best_features = []
         best_accuracy = 0
         num_features = self.validator.data.shape[1]
-        last_best_accuracy = 0
+        highest_accuracy = best_accuracy
+        highest_accuracy_features = best_features.copy()
 
-        for i in range(num_features):
+        while len(best_features) < num_features:
             current_best_feature = None
-            current_best_accuracy = 0
+            current_best_accuracy = -1  # Initialize to a value less than possible accuracy
 
             for feature in range(num_features):
                 if feature in best_features:
                     continue
 
-                accuracy = self.validator.validate(self.classifier, best_features + [feature])
-                print(f"Using feature(s) {[f+1 for f in best_features] + [feature+1]} accuracy is {accuracy:.2f}")
+                feature_set = best_features + [feature]
+                accuracy = self.validator.validate(self.classifier, feature_set)
+                print(f"Using feature(s) {[f+1 for f in feature_set]} accuracy is {accuracy:.2f}")
 
                 if accuracy > current_best_accuracy:
                     current_best_accuracy = accuracy
                     current_best_feature = feature
 
-            if current_best_accuracy - last_best_accuracy < min_accuracy_improvement:
-                print("Accuracy has not improved. Stopping.")
-                break
-
-            last_best_accuracy = current_best_accuracy
-
-            if current_best_accuracy > best_accuracy:
-                best_accuracy = current_best_accuracy
+            if current_best_feature is not None:
                 best_features.append(current_best_feature)
+                best_accuracy = current_best_accuracy
+                print(f"Feature set {[f+1 for f in best_features]} was best, accuracy is {best_accuracy:.2f}\n")
 
-            print(f"Feature set {[f+1 for f in best_features]} was best, accuracy is {best_accuracy:.2f}\n")
+                if best_accuracy > highest_accuracy:
+                    highest_accuracy = best_accuracy
+                    highest_accuracy_features = best_features.copy()
 
-        print(f"\nBest feature subset (forward selection): {[f+1 for f in best_features]} with accuracy: {best_accuracy:.2f}\n")
+        print(f"\nBest feature subset (forward selection): {[f+1 for f in highest_accuracy_features]} with accuracy: {highest_accuracy:.2f}\n")
+
 
     def backwardElimination(self):
         num_features = self.validator.data.shape[1]
@@ -48,10 +49,12 @@ class FeatureSelection:
         best_accuracy = self.validator.validate(self.classifier, best_features)
         print(f"\nStarting with feature set {[f+1 for f in best_features]} with accuracy {best_accuracy:.2f}")
 
+        highest_accuracy = best_accuracy
+        highest_accuracy_features = best_features.copy()
+
         while len(best_features) > 1:
-            current_best_feature_set = best_features
-            current_best_accuracy = best_accuracy
-            improvement = False
+            current_best_accuracy = -1
+            current_worst_feature = None
 
             for feature in best_features:
                 feature_set = [f for f in best_features if f != feature]
@@ -60,81 +63,87 @@ class FeatureSelection:
 
                 if accuracy > current_best_accuracy:
                     current_best_accuracy = accuracy
-                    current_best_feature_set = feature_set
-                    improvement = True
+                    current_worst_feature = feature
 
-            if improvement:
+            if current_worst_feature is not None:
+                best_features.remove(current_worst_feature)
                 best_accuracy = current_best_accuracy
-                best_features = current_best_feature_set
                 print(f"Feature set {[f+1 for f in best_features]} was best, accuracy is {best_accuracy:.2f}\n")
-            else:
-                break
 
-        print(f"\nBest feature subset (backward elimination): {[f+1 for f in best_features]} with accuracy: {best_accuracy:.2f}")
+                if best_accuracy > highest_accuracy:
+                    highest_accuracy = best_accuracy
+                    highest_accuracy_features = best_features.copy()
+
+        print(f"\nBest feature subset (backward elimination): {[f+1 for f in highest_accuracy_features]} with accuracy: {highest_accuracy:.2f}")
 
     def SCVforwardSelection(self):
         best_features = []
         remaining_features = list(range(self.validator.data.shape[1]))
-        best_accuracy = 0
-        last_best_accuracy = 0
+        highest_accuracy = 0
+        highest_accuracy_features = best_features.copy()
         min_accuracy_improvement = 0.001
 
         while remaining_features:
-            best_feature = None
-            current_best_accuracy = 0
+            current_best_feature = None
+            current_best_accuracy = -1  # Initialize to a value less than possible accuracy
 
             for feature in remaining_features:
-                current_features = best_features + [feature]
-                accuracy = self.validator.stratified_cross_validation(self.classifier, current_features)
-                print(f"Using feature(s) {[f+1 for f in best_features] + [feature+1]} accuracy is {accuracy:.2f}")
+                feature_set = best_features + [feature]
+                accuracy = self.validator.stratified_cross_validation(self.classifier, feature_set)
+                print(f"Using feature(s) {[f+1 for f in feature_set]} accuracy is {accuracy:.2f}")
 
                 if accuracy > current_best_accuracy:
                     current_best_accuracy = accuracy
-                    best_feature = feature
+                    current_best_feature = feature
 
-            if current_best_accuracy - last_best_accuracy < min_accuracy_improvement:
-                print("Accuracy has not improved. Stopping.")
-                break
-
-            last_best_accuracy = current_best_accuracy
-
-            if best_feature is not None:
-                best_features.append(best_feature)
-                remaining_features.remove(best_feature)
+            if current_best_feature is not None:
+                best_features.append(current_best_feature)
+                remaining_features.remove(current_best_feature)
                 print(f"Feature set {[f+1 for f in best_features]} was best, accuracy is {current_best_accuracy:.2f}\n")
+
+                if current_best_accuracy - highest_accuracy >= min_accuracy_improvement:
+                    highest_accuracy = current_best_accuracy
+                    highest_accuracy_features = best_features.copy()
             else:
                 break
 
-        print(f"\nBest feature subset (SCV forward selection): {[f+1 for f in best_features]} with accuracy: {current_best_accuracy:.2f}")
+        print(f"\nBest feature subset (SCV forward selection): {[f+1 for f in highest_accuracy_features]} with accuracy: {highest_accuracy:.2f}\n")
+
 
     def SCVbackwardElimination(self):
-        best_features = list(range(self.validator.data.shape[1]))
+        num_features = self.validator.data.shape[1]
+        best_features = list(range(num_features))
         best_accuracy = self.validator.stratified_cross_validation(self.classifier, best_features)
         print(f"\nStarting with feature set {[f+1 for f in best_features]} with accuracy {best_accuracy:.2f}")
 
+        highest_accuracy = best_accuracy
+        highest_accuracy_features = best_features.copy()
+
         while len(best_features) > 1:
-            current_best_feature_set = best_features
-            current_best_accuracy = best_accuracy
-            improvement = False
+            current_best_accuracy = -1
+            current_worst_feature = None
 
             for feature in best_features:
-                current_features = [f for f in best_features if f != feature]
-                accuracy = self.validator.stratified_cross_validation(self.classifier, current_features)
-                print(f"Using feature(s) {[f+1 for f in current_features]} accuracy is {accuracy:.2f}")
+                feature_set = [f for f in best_features if f != feature]
+                accuracy = self.validator.stratified_cross_validation(self.classifier, feature_set)
+                print(f"Using feature(s) {[f+1 for f in feature_set]} accuracy is {accuracy:.2f}")
 
                 if accuracy > current_best_accuracy:
                     current_best_accuracy = accuracy
-                    current_best_feature_set = current_features
-                    improvement = True
+                    current_worst_feature = feature
 
-            if improvement:
+            if current_worst_feature is not None:
+                best_features.remove(current_worst_feature)
                 best_accuracy = current_best_accuracy
-                best_features = current_best_feature_set
                 print(f"Feature set {[f+1 for f in best_features]} was best, accuracy is {best_accuracy:.2f}\n")
-            else:
-                break
 
-        print(f"\nBest feature subset (SCV backward elimination): {[f+1 for f in best_features]} with accuracy: {best_accuracy:.2f}")
+                if best_accuracy > highest_accuracy:
+                    highest_accuracy = best_accuracy
+                    highest_accuracy_features = best_features.copy()
+
+        print(f"\nBest feature subset (SCV backward elimination): {[f+1 for f in highest_accuracy_features]} with accuracy: {highest_accuracy:.2f}")
+
+
 
     def BobAlgo(self, n_components=5):
         print("\nRunning Bob the Builder's Special Algo (SVD)...")
